1046bbbd006ccbcce464c16caadf478f3e0413d6
Ratanak Panha Duong <ratanakpanhaduong@Ratanaks-MacBook-Pro.local>
2024-06-29 15:53:16 +1000

    Posts and Users

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,15 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    # IO.puts("Users function hit!")
+    users = [
+      %{id: 1, name: "Alice", email: "alice@email.com"},
+      %{id: 2, name: "Bob", email: "bob@email.com"}
+    ]
+
+    # render(conn, :users, users: users, layout: false)
+    json(conn, %{users: users})
+  end
 end
